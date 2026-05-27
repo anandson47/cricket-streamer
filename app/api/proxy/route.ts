0e351a62ddd6d91dfcc6151fccdbe96d9d4f9530
anand vijay <anandson47@gmail.com>
2026-05-27 18:35:24 +0530

    header switch

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -16,13 +16,15 @@ export async function GET(req: NextRequest) {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+
         Accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+
         "Accept-Language": "en-US,en;q=0.9",
+
         Referer: "https://cricheroes.com/",
+
         Origin: "https://cricheroes.com",
-        Connection: "keep-alive",
-        "Cache-Control": "no-cache",
       },
     });
 
